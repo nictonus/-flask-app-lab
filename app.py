03854f15e94887063dda50197ac7c21b6f852030
nictonus <64956382+nictonus@users.noreply.github.com>
2025-01-30 02:56:51 +0200

    22
    22

--- app.py
+++ app.py
@@ -1,37 +1,57 @@
-from flask import Flask, request, redirect, url_for, render_template
-from werkzeug.utils import redirect
+from flask import Flask, request, redirect, url_for, render_template, abort
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 
-@app.route('/')  # URL '/' to be handled by main() route handler
+@app.route('/')
 def main():
-    return redirect(url_for('resume'))
+    return render_template("hello.html")
 
 
 @app.route('/homepage')
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
-    return f"<h1>This is your homepage :) - {agent}</h1> "
+
+    return render_template("home.html", agent=agent)
 
 
-@app.route('/hi/<string:name>') #/hi/Andriy?age=20
+@app.route("/hi/<string:name>")  # /hi/ivan?age=45&q=fdfdf
 def greetings(name):
     name = name.upper()
-    age = request.args.get("age", 0, type=int)
-    return f"Welcome {name=} {age=}", 200
+    age = request.args.get("age", None, int)
 
-@app.route('/admin')
+    return render_template("hi.html",
+                           name=name, age=age)
+
+
+@app.route("/admin")
 def admin():
-   to_url = url_for("greetings", name="Administrator", _external=True)            #"/hi/admin"
-   print(to_url)
-   return redirect(to_url)
+    to_url = url_for("greetings", name="administrator", age=45,
+                     _external=True)  # "http://localhost:8080/hi/administrator?age=45"
+    print(to_url)
+    return redirect(to_url)
 
-@app.route('/resume')
-def resume():
-    return render_template('resume.html', title="Резюме")
+
+posts = [
+    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+
+@app.route('/posts')
+def get_posts():
+    return render_template("posts.html", posts=posts)
+
+
+@app.route('/post/<int:id>')
+def detail_post(id):
+    if id > 3:
+        abort(404)
+    post = posts[id - 1]
+    return render_template("detail_post.html", post=post)
 
 
 if __name__ == "__main__":
