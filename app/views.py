b7a03784219d001cbd1922cc7fb193b3fc34dd2b
nictonus <64956382+nictonus@users.noreply.github.com>
2025-01-30 02:49:58 +0200

    4
    4

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from flask import request, redirect, url_for, render_template, abort
+from . import app
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+    agent = request.user_agent
+
+    return render_template("home.html", agent=agent)
+
+@app.route('/resume')
+def show_resume():
+    return render_template("resume.html")
+
+
+
